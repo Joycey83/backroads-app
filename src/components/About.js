36bcde1593037d9e2487cded2dd5503c565d7e2d
Joycey <Jcheung8385@gmail.com>
2023-03-28 18:09:55 +0100

    fixed the build bug by adding CI= so that the site will deploy on netlify, also added local-build so that we can see the website on local server

--- src/components/About.js
+++ src/components/About.js
@@ -23,7 +23,7 @@ const About = () => {
               Aspernatur quisquam harum nam cumque temporibus explicabo dolorum
               sapiente odio unde dolor?
             </p>
-            <a href="#" className="btn">
+            <a href="#home" className="btn">
               read more
             </a>
           </article>
